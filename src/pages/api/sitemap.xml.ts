233f7abfa110a64dc3b2a4391df204db023a8d21
MotohiroSeki <149162146+MotohiroSeki@users.noreply.github.com>
2024-08-15 22:24:27 +0900

    #15 modify sitemap

--- src/pages/api/sitemap.xml.ts
+++ src/pages/api/sitemap.xml.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/xml');
-  res.setHeader('Cache-control', 'stale-while-revalidate, s-maxage=3600');
+  res.setHeader('Cache-Control', 'stale-while-revalidate, s-maxage=3600');
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
